c5c98b8241a56e87774a1f6a74cc73fa3c18758f
pranjaldby <dpranjal.088@gmail.com>
2026-02-20 19:29:29 +0530

    first commit pushing portfolio files

---
+++ userlogin/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class UserInfo(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    username=models.CharField('username',max_length=50,null=False,unique=True)
+    password_hash=models.TextField('password',null=False)
+    time_stamp = models.DateTimeField(auto_now_add=True)
+    
+    # creating a Meta class to tell django that do not overwrite this table it is already created
+    class Meta:
+        db_table = 'user_info'
+        managed = False
+        
+        
+        
+
+    
