8361ce3764e746bcb1bf25d718af10bb657347db
pranjaldby <dpranjal.088@gmail.com>
2026-02-22 11:39:28 +0530

    change db engine to sqlite3

--- DjangoWebapp/settings.py
+++ DjangoWebapp/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'DjangoWebapp.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+  'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
