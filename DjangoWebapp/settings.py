7badbd6e8bc9dc3e80781e54ff1850afb8560055
pranjaldby <dpranjal.088@gmail.com>
2026-02-22 19:05:09 +0530

    set debug to true

--- DjangoWebapp/settings.py
+++ DjangoWebapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_&w6@v$nu@dbt!$ct3-=5kr0v_plu=vg$8cdd$qklpcmmk&##r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS += ['.elasticbeanstalk.com']
