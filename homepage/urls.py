be8298a5c1b3bf90423bdf678a498f71e2dbec11
pranjaldby <dpranjal.088@gmail.com>
2026-02-27 10:25:48 +0530

    fixed / redirect error

--- homepage/urls.py
+++ homepage/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('health/',views.health),
     path('homepage/',views.homepage,name='homepage'),
-    path('homepage/about/',views.about)
+    path('homepage/about/',views.about,name='about')
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render,redirect
 from userlogin import models
 # Create your views here.
 
+#fixing elastic bean health issue
+from django.http import HttpResponse
+def health(request):
+    if "user_id" not in request.session:
+        return redirect('')
+    return HttpResponse("OK")
+
 def homepage(request):
     # use Django shortcut to render the template and ensure request is used
     if "user_id" not in request.session:
